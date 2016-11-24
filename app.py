ebb5b64b5a121175dcf8592709196efa25cfa9f3
ramirobg94 <ramirobg94@gmail.com>
2016-11-24 14:10:59 +0100

    Git problems

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask import request
 from celery import Celery
+from git import Repo
+
 
 
 def make_celery(app):
@@ -32,12 +35,30 @@ def add(x, y):
     print(x)
     return x + y
 
+@celery.task(name="checkertask")
+def checkertask(lang, repo, type):
+    print(lang)
+    print(repo)
+    print(type)
+    g = git.cmd.Git('git@github.com:ramirobg94/pruebaR.git')
+    d.pull()
+    return 2
+
+
 
 @app.route("/")
 def hello():
     celery.send_task("mytask", args=(2, 3))
-    
     return "hola"
+
+
+@app.route("/check")
+def check():
+    lang = request.args['lang']
+    repo = request.args['repo']
+    type = request.args['type']
+    celery.send_task("checkertask", args=(lang, repo, type))
+    return "checking"
 
 
 if __name__ == "__main__":
