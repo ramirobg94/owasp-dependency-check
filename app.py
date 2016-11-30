595584d5b0e4a3eb3cf51caddca857d97d38d048
ramirobg94 <ramirobg94@gmail.com>
2016-11-30 10:24:40 +0100

    Git problems solved

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask
 from flask import request
 from celery import Celery
-from git import Repo
+#from urllib.parse import urlencode
+#from urllib.parse import urlparse
+#from gittle import Gittle
+import os
+from subprocess import call
+import time
+import junitxml
 
-
+#repo_path = '/tmp/tmpRepo'
+#repo_url = 'git://github.com/FriendCode/gittle.git'
+#repo_path_b = repo_path.encode(encoding="ascii")
+#repo_url_b = repo_url.encode(encoding="ascii")
+#repo = Gittle.clone(repo_url_b, repo_path)
 
 def make_celery(app):
     celery = Celery(app.import_name, backend=app.config['CELERY_BACKEND'],
@@ -37,14 +47,17 @@ def add(x, y):
 
 @celery.task(name="checkertask")
 def checkertask(lang, repo, type):
-    print(lang)
-    print(repo)
-    print(type)
-    g = git.cmd.Git('git@github.com:ramirobg94/pruebaR.git')
-    d.pull()
+
+    path = repo.split('/')[-1]
+    name = path[:-4]
+    path = '/tmp/repoTmpo/' + name
+    os.system('git clone '+repo+' '+path)
+    os.system('ls '+path)
+    os.system('dependency-check --project "'+ repo +'"  --scan "'+ path +'" -f "HTML" -o  "'+ path +'" --enableExperimental')
+    #fp = file('results.xml', 'wb')
+    #result = junitxml.JUnitXmlResult(fp)
+    os.system('ls ' + path)
     return 2
-
-
 
 @app.route("/")
 def hello():
