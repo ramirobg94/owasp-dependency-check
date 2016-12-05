fc6f160b5d31dc3e537e0af19055f530f8715eff
ramirobg94 <ramirobg94@gmail.com>
2016-12-05 11:04:06 +0100

    Vulnerability class

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from celery import Celery
 import os
 from subprocess import call
 import time
+from vulnerability import Vulnerability
+import xml.etree.ElementTree as ET
 
 #repo_path = '/tmp/tmpRepo'
 #repo_url = 'git://github.com/FriendCode/gittle.git'
@@ -52,7 +54,15 @@ def checkertask(lang, repo, type):
     path = '/tmp/repoTmpo/' + name
     os.system('git clone '+repo+' '+path)
     os.system('ls '+path)
-    os.system('dependency-check --project "'+ repo +'"  --scan "'+ path +'" -f "HTML" -o  "'+ path +'" --enableExperimental')
+    os.system('dependency-check --project "'+ repo +'"  --scan "'+ path +'" -f "XML" -o  "'+ path +'" --enableExperimental')
+
+    tree = ET.parse(path + '/dependency-check-report.xml')
+    root = tree.getroot()
+    print(root)
+    for vulnerability in root.findall('./dependencies'):
+        print(vulnerability)
+        #name = vulnerability.find('name').text
+        #print(name)
     #fp = file('results.xml', 'wb')
     #result = junitxml.JUnitXmlResult(fp)
     os.system('ls ' + path)
@@ -74,7 +84,6 @@ def retiretask(lang, repo, type):
     os.chdir(path)
     os.system('npm install')
     os.system('retire --outputformat text --outputpath '+ path +'/checkba.txt')
-    #os.system('ls '+path)
     #fp = file('results.xml', 'wb')
     #result = junitxml.JUnitXmlResult(fp)
     #os.system('ls ' + path)
@@ -85,14 +94,17 @@ def retiretask(lang, repo, type):
     for x in f:
         if "has known vulnerabilities" in x:
             parsed = x.split(" ")
-            default = ["no library", "no version", "no level", "no summary", "no advisory"]
-            default[0] = parsed[0]
-            default[1] = parsed[1]
+            default = ["no library", "no version", "no severity", "no summary", "no advisory"]
+            library = parsed[0]
+            version = parsed[1]
             if "severity" in x:
-                default[2] = parsed[7]
-            default[3] = x[x.find("summary"):].replace("\n", '')
-            default[4] = x[x.find("advisory"):].replace("\n", '')
-            cleared_results.append(default)
+                severity = parsed[7]
+            else:
+                severity = ''
+            summary  = x[x.find("summary"):].replace("\n", '')
+            advisory = x[x.find("advisory"):].replace("\n", '')
+            vulnerability = Vulnerability(library, version, severity, summary, advisory)
+            cleared_results.append(vulnerability)
 
     return 2
 
@@ -108,8 +120,8 @@ def check():
     lang = request.args['lang']
     repo = request.args['repo']
     type = request.args['type']
-    #celery.send_task("checkertask", args=(lang, repo, type))
-    celery.send_task("retiretask", args=(lang, repo, type))
+    celery.send_task("checkertask", args=(lang, repo, type))
+    #celery.send_task("retiretask", args=(lang, repo, type))
     return "checking"
 
 
