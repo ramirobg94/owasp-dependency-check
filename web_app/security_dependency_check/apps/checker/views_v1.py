c6bd5d76494592cd174a755566d13e9f07cf37fa
ramirobg94 <ramirobg94@gmail.com>
2017-01-17 13:20:17 +0100

    debuging preDemo

--- web_app/security_dependency_check/apps/checker/views_v1.py
+++ web_app/security_dependency_check/apps/checker/views_v1.py
@@ -34,7 +34,7 @@ def check():
     db.session.add(project)
     db.session.commit()
     
-    #celery.send_task("owasp_dependency_checker_task", args=(lang, repo, type, project.id))
+    celery.send_task("owasp_dependency_checker_task", args=(lang, repo, type, project.id))
     celery.send_task("retire_task", args=(lang, repo, type, project.id))
 
     return "checking project {} you can check the status at: /status/{}".format(str(project.id), str(project.id))
@@ -44,7 +44,8 @@ def check():
 def status(project_id):
     """Check the state and vulnerability of the project"""
     project = Project.query.filter_by(id=project_id).one()
-    # vuls = Vulnerabilities.query.filter_by(project_id = project_id)
+    #vuls = Vulnerabilities.query.filter_by(project_id = project_id)
+    #vuls = project.vulnerabilities
 
     status = project.numberTests - project.passedTests
     if status == 0:
